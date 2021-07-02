77816475f31e211295a0b6009e8f754bd1b050d4
multiahfoon <multiahfoon@gmail.com>
2021-07-03 01:44:19 +1200

    build(app): convert component to class based

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+// import react so when bundled ejx syntax can be converted into html
+// import component to convert function component to class based 
+import React, {Component} from 'react'
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+// create class called App which extends from component
+class App extends Component  {
+  render() {
+    return (
+      <div>Hello World</div>
+    )
+  }
 }
 
+// export app component
 export default App;
