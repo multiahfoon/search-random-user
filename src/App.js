dec47c3fc4c7df06e00db0fd204126b5d2ccf317
multiahfoon <multiahfoon@gmail.com>
2021-07-03 07:21:39 +1200

    feat(app): add user info to state
    An api call is made when app component is mounted.
    The response is then added to state as user data.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@
 // import component to convert function component to class based 
 import React, {Component} from 'react'
 
+// import components
+import { CardList } from './components/card-list/CardList';
+
 import './App.css';
 
 // create class called App which extends from component
 class App extends Component  {
+
+  constructor() {
+    super()
+    this.state = ({
+      users: [],
+      search: ''
+    })
+  }
+
+  componentDidMount() {
+    fetch('https://randomuser.me/api/?results=8')
+    .then(response => response.json())
+    .then(users => this.setState({users: users.results}))
+  }
+
   render() {
     return (
-      <div>Hello World</div>
+      <div> 
+        <CardList users={this.state.users}/>
+      </div>
     )
   }
 }
