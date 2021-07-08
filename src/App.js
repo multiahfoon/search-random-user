d45b386fb6a927dffa8ebfbe9ae60f74c49de3ed
multiahfoon <multiahfoon@gmail.com>
2021-07-09 04:07:30 +1200

    style(refactor): update component to use hooks
    App is no longer a class component due to using
    react hooks to handle effects of mounting and state

--- src/App.js
+++ src/App.js
@@ -1,43 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { CardList } from './components/card-list/CardList';
 import { Search } from './components/search/Search';
 import './App.css';
 
-class App extends Component {
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [searchUser, setSearchUser] = useState('')
 
-  constructor() {
-    super()
-    this.state = ({
-      users: [],
-      userSearch: ''
-    })
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://randomuser.me/api/?results=8')
       .then(response => response.json())
-      .then(users => this.setState({ users: users.results }))
+      .then(userData => setUsers(userData.results))
+      .catch(err => console.log(err))
+  }, [])
+
+  const handleChange = (e) => {
+    setSearchUser(e.target.value)
   }
 
-  handleChange = (e) => {
-    return this.setState({ userSearch: e.target.value })
-  }
+  const filter = users.filter(user => {
+    return user.name.first.toLowerCase().includes(searchUser.toLowerCase())
+  })
 
-  render() {
-    const { users, userSearch } = this.state
-    const filter = users.filter(user => {
-      return user.name.first.toLowerCase().includes(userSearch.toLowerCase())
-    })
-
-
-    return (
-      <div>
-        < Search handleChange={this.handleChange} />
-        <CardList users={filter} />
-      </div >
-    )
-  }
+  return (
+    <div>
+      <Search handleChange={handleChange} />
+      <CardList users={filter} />
+    </div >
+  )
 }
 
 export default App;
